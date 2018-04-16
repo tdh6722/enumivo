03d095d825d58f7ad13c02c39918b313f5fedd21
enumivo <admin@enumivo.org>
2018-04-16 10:10:03 +0800

    remove docs (just rebuild it later)

--- docs/search/namespaces_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['account',['account',['../namespaceeosio_1_1account.html',1,'eosio']]],
-  ['eosio',['eosio',['../namespaceeosio.html',1,'']]],
-  ['enumivosystem',['enumivosystem',['../namespaceenumivosystem.html',1,'']]]
-];
