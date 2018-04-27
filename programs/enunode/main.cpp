8e8d91b74e82d1d9735c61d9c5c9d212588e8678
enumivo <admin@enumivo.org>
2018-04-27 09:50:50 +0800

    enunode to enumivo

--- programs/enunode/main.cpp
+++ programs/enunode/main.cpp
@@ -4,11 +4,11 @@
  */
 #include <appbase/application.hpp>
 
-#include <enunode/chain_plugin/chain_plugin.hpp>
-#include <enunode/http_plugin/http_plugin.hpp>
-#include <enunode/net_plugin/net_plugin.hpp>
-#include <enunode/producer_plugin/producer_plugin.hpp>
-#include <enunode/utilities/common.hpp>
+#include <enumivo/chain_plugin/chain_plugin.hpp>
+#include <enumivo/http_plugin/http_plugin.hpp>
+#include <enumivo/net_plugin/net_plugin.hpp>
+#include <enumivo/producer_plugin/producer_plugin.hpp>
+#include <enumivo/utilities/common.hpp>
 
 #include <fc/log/logger_config.hpp>
 #include <fc/log/appender.hpp>
